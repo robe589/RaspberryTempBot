a5f3838ed80a7cc04623bfc23b51d287add3e62e
yoshiaki <ikimono.miwa589@gmail.com>
2015-06-06 23:09:05 +0900

    CPU温度を取得し、twitterに送る処理を追加

--- main.rb
+++ main.rb
@@ -9,10 +9,13 @@ def main()
 		config.consumer_key=$consumer_key#API Key
 		config.consumer_secret=$consumer_secret#API secret
 		config.access_token=$access_token #Access token
-		0config.access_token_secret=$access_token_secret  #Access token secret
+		config.access_token_secret=$access_token_secret  #Access token secret
 	end
-	
-	client.update("test")
+	temp=`cat /sys/class/thermal/thermal_zone0/temp`
+	temp.slice!(2..-1)
+	str=Time.now.strftime("%Y年%m月%d日%H時の温度は")+temp+'度です'
+	p str
+	client.update(str)
 end
 
 main()
