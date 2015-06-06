667c387589910b59fc27f0cd03623fad5074118c
yoshiaki <ikimono.miwa589@gmail.com>
2015-06-06 19:22:03 +0900

    first commit

---
+++ main.rb
@@ -0,0 +1,18 @@
+#coding: utf-8
+require 'bundler'
+Bundler.require
+
+require './myid'
+
+def main()
+	client = Twitter::REST::Client.new do |config|
+		config.consumer_key=$consumer_key#API Key
+		config.consumer_secret=$consumer_secret#API secret
+		config.access_token=$access_token #Access token
+		0config.access_token_secret=$access_token_secret  #Access token secret
+	end
+	
+	client.update("test")
+end
+
+main()
