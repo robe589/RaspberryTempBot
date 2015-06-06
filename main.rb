241a6fbd4053e844bc0249bba9c8d0faa3ad39d8
yoshiaki <ikimono.miwa589@gmail.com>
2015-06-06 23:19:49 +0900

    ツイートする内容に何分かもわかるように追加

--- main.rb
+++ main.rb
@@ -5,15 +5,15 @@ Bundler.require
 require './myid'
 
 def main()
+	temp=`cat /sys/class/thermal/thermal_zone0/temp`
 	client = Twitter::REST::Client.new do |config|
 		config.consumer_key=$consumer_key#API Key
 		config.consumer_secret=$consumer_secret#API secret
 		config.access_token=$access_token #Access token
 		config.access_token_secret=$access_token_secret  #Access token secret
 	end
-	temp=`cat /sys/class/thermal/thermal_zone0/temp`
 	temp.slice!(2..-1)
-	str=Time.now.strftime("%Y年%m月%d日%H時の温度は")+temp+'度です'
+	str=Time.now.strftime("%Y年%m月%d日%H時%M分の温度は")+temp+'度です'
 	p str
 	client.update(str)
 end
