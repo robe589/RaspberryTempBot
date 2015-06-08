c20bd1f4deeeee2a381036b7050b9884dde89ae1
yoshiaki <ikimono.miwa589@gmail.com>
2015-06-08 22:35:54 +0900

    コメントを追加

--- main.rb
+++ main.rb
@@ -9,14 +9,17 @@ def main()
 	temp.slice!(3..-1)
 	temp.insert(2,".")
 	str=Time.now.strftime("%Y年%m月%d日%H時%M分の温度は")+temp+'度です'
+	
+	#ツイッターログイン処理
 	client = Twitter::REST::Client.new do |config|
 		config.consumer_key=$consumer_key#API Key
 		config.consumer_secret=$consumer_secret#API secret
 		config.access_token=$access_token #Access token
-		config.access_token_secret=$access_token_secret  #Access token secret
+		config.access_token_secret=$access_token_secret #Access token secret
 	end
 
 	p str
+	#ツイッターに投稿
 	client.update(str)
 end
 
