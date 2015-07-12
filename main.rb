48784914828f4dc2a19ca8e1f5ba65e6aa78a1ea
yoshiaki <ikimono.miwa589@gmail.com>
2015-07-12 16:50:42 +0900

    ツイッターに投稿する処理をTwitterApiクラスに置き換え

--- main.rb
+++ main.rb
@@ -3,21 +3,16 @@ require 'bundler'
 Bundler.require
 
 require './myid'
+require './TwitterApi'
 
 def main()
 	temp=`cat /sys/class/thermal/thermal_zone0/temp`
 	temp.slice!(3..-1)
 	temp.insert(2,".")
 	str=Time.now.strftime("%Y年%m月%d日%H時%M分の温度は")+temp+'度です'
-	client = Twitter::REST::Client.new do |config|
-		config.consumer_key=$consumer_key#API Key
-		config.consumer_secret=$consumer_secret#API secret
-		config.access_token=$access_token #Access token
-		config.access_token_secret=$access_token_secret  #Access token secret
-	end
-
-	p str
-	client.update(str)
+	#twitterに投稿
+	twitterApi=TwitterApi.new($consumer_key,$consumer_secret,$access_token,$access_token_secret)
+	twitterApi.write(str)
 end
 
 main()
